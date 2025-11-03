ac8405b72376653260b50f33d452ed92626979d2
Kuria Gatonye <stevekuriagatonye@gmail.com>
2025-11-03 23:33:26 +0300

    Finance calculator for monthly and annual interest inclusive savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,10 @@
-monthly_income = int(input("Enter your monthly income "))
-monthly_expenses = int(input("Enter your total monthly expenses "))
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
 
+#Calculates monthly savings
 Monthly_Savings  = monthly_income - monthly_expenses
 
+#Calculates projected annual savings with 0.05 interest
 Projected_Savings =  Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
 
 print(f"Your monthly savings are {Monthly_Savings}")
