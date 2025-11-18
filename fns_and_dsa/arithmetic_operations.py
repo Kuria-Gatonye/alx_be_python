76c4c4bda161c78b902339682f281ec242a9331b
Kuria Gatonye <stevekuriagatonye@gmail.com>
2025-11-18 15:45:27 +0300

    Created an arithmetic function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,26 +1,18 @@
-def perform_operation():
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    if operation == "add":
-        result = num1 + num2
-        print(f"Result: {result}")
+def perform_operation(num1, num2, operation):
     
-    elif operation == "-":
-        result = num1 - num2
-        print(f"Result: {result}")
+    if operation == "add":
+        return num1 + num2
+    
+    elif operation == "subtract":
+        return num1 - num2
+    
+    elif operation == "multiply":
+        return num1 * num2
 
-    elif operation == "*":
-        result = num1 * num2
-        print(f"Result: {result}")
-
-    elif operation == "/":
+    elif operation == "divide":
         if num2 == 0:
             print("Cannot divide by zero")
 
         else:
-            result = num1 / num2
-            print(f"Result: {result}")
+            return num1/num2
 
-perform_operation()   
