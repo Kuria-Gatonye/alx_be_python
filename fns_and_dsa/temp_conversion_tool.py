f6887b5038a4df6ae114f5ed41747598e10e9485
Kuria Gatonye <stevekuriagatonye@gmail.com>
2025-11-18 20:11:23 +0300

    Explored variable scopes bt creating a temperature conversion tool

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,25 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+
+def convert_to_fahrenheit(celsius):
+    fahrenheit = (celsius  * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return fahrenheit
+
+temp = float(input("Enter the temperature to convert: "))
+mode = input("Is this temperature in Celsius or Fahrenheit? (C/F)")
+
+if mode == "C":
+    converted = convert_to_celsius(temp)
+    print(f"{temp}°F is {converted}°C")
+
+elif mode == "F":
+    converted = convert_to_fahrenheit(temp)
+    print(f"{temp}°C is {converted}°F")
+
+else:
+    print("No mode was specified")
