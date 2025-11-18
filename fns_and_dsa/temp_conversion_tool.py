f4c38da57b0eb40024ec67895573b1339519a3cf
Kuria Gatonye <stevekuriagatonye@gmail.com>
2025-11-18 20:36:50 +0300

    Explored variable scopes bt creating a temperature conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,9 @@
+#global variables
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
+#functions
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
@@ -10,16 +12,22 @@ def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius  * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
 
-temp = float(input("Enter the temperature to convert: "))
-mode = input("Is this temperature in Celsius or Fahrenheit? (C/F)")
-
-if mode == "C":
-    converted = convert_to_celsius(temp)
-    print(f"{temp}°F is {converted}°C")
-
-elif mode == "F":
-    converted = convert_to_fahrenheit(temp)
-    print(f"{temp}°C is {converted}°F")
-
+#User input
+temp = int(input("Enter the temperature to convert: "))
+if not temp.is_integer():
+    print("Invalid temperature. Please enter a numeric value.")
 else:
-    print("No mode was specified")
+    mode = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+    if mode == "C":
+        converted = convert_to_fahrenheit(temp)
+        print(f"{temp}°C is {converted}°F")
+        
+    elif mode == "F":
+        converted = convert_to_celsius(temp)
+        print(f"{temp}°F is {converted}°C")
+
+    else:
+        print("No mode was specified")
+
+    
